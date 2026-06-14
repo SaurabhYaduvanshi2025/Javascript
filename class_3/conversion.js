c3deeb294fc3d288ca7a101ced0f099af707e3d2
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-06-14 16:59:49 +0530

    Add  new File conversion.js

---
+++ class_3/conversion.js
@@ -0,0 +1,34 @@
+str1 = 1;
+
+str2 = 4;
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1"  + 4);
+
+console.log(5 + "5")
+
+console.log((5*4) + 4 -5)
+
+
+console.log(1 + 2 + "4")
+
+
+
+let x = 4;
+
+const y = x++;
+
+console.log(`x:${x},y:${y}`);
+
+
+let a = 7 ;
+
+const b = ++a ;
+
+console.log(`a:${a},b${b}`);
+
+
+
