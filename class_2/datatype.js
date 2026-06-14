cf11c9211447dbd6b33a63a901dde5a1a885ede2
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-06-14 09:34:04 +0530

    Add code in directory

---
+++ class_2/datatype.js
@@ -0,0 +1,40 @@
+"use strict";
+
+//alert (6+2) out put never show because we use nodejs , not a browser 
+
+console.log(3+3)
+
+// let score = true;
+// let target = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// let valueOfNumber = Number(target);
+// console.log(typeof valueOfNumber);
+
+
+
+let isLoggedIn = "";
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsloggedIn)
+
+
+
+// 1 => true; 0 => false;
+
+// "" => false
+
+// "saurabh" => true;
+
+
+let someRandomNumber = 85;
+
+let stringNumber = String(someRandomNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber);
