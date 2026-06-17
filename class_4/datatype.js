bfe2066c162e8a942334f66d3280efb557ca4b08
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-06-17 06:54:11 +0530

    add here class 4

---
+++ class_4/datatype.js
@@ -0,0 +1,31 @@
+//Primitive
+
+//7 types : String , Number , Boolean , Null Undefined , Symbol, BigInt
+
+
+const score = 100 ;
+
+const scoreValue = 100.3 ;
+
+const isLoggedIn = false
+const outsideTemp = null ;
+
+let userEmail;
+
+
+const id = Symbol ('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 4578968574123654n
+
+//Reference (Non primitive)
+
+// Array , Objects , Function 
+
+const heros = ["Shaktiman ", "Naagraj" , "Doga"];
+let  myObj = {
+    name:"Saurabh",
+    age:25,
+}
