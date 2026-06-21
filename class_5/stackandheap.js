8598b2e22979622df88511dc8025337f462513ef
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-06-21 13:59:19 +0530

    Add class 5

---
+++ class_5/stackandheap.js
@@ -0,0 +1,17 @@
+// ***************************************************************************
+// Stack and Heap in JavaScript
+// ***************************************************************************
+
+// Stack and Heap are two types of memory used in programming languages like JavaScript. Understanding how they work can help you write more efficient code and avoid common pitfalls.
+            
+// Stack:(Primitives) (int , string, boolean , float , double, null, undefined) , Heap : (Non-Primitives) (objects, arrays, functions)
+
+
+let myYoutubename = "histeshchoudharydotcom";
+
+let anotherYoutubename = myYoutubename;
+
+anotherYoutubename = "chaiorcode";
+
+console.log(myYoutubename); // Output: histeshchoudharydotcom
+console.log(anotherYoutubename); // Output: chaiorcode
