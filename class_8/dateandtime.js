f13811d181493c7d7398f7c18fb8aa28d925b24a
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-06-28 23:07:43 +0530

    Add class 8th

---
+++ class_8/dateandtime.js
@@ -0,0 +1,14 @@
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// let myCreatedDate = new Date("01-06-2026");
+// console.log(myCreatedDate.toString());
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toLocaleString());
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(myTimeStamp.floor(myTimeStamp / 1000));
