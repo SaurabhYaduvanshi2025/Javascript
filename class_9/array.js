a8fe91d7ba273fbb97eaed2bf805188910b381e4
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-07-31 23:09:16 +0530

    Practice in Array file

--- class_9/array.js
+++ class_9/array.js
@@ -26,6 +26,36 @@ const myHeros = ["Ranbir Kapoor", "Iron Man", "Captain America", "Shaktiman"];
 // console.log(sliceArray);
 // console.log(myArray);
 
-const newArray = myArray.splice(1 , 4);
-console.log(myArray);
+// const newArray = myArray.splice(1 , 4);
+// console.log(myArray);
 // console.log(newArray)
+
+
+// const number1 = [1,2,3,4,5,6];
+// const number2 = [2,3,4,5,6,7];
+// const number3 = [3,4,5,6,7,7];
+
+// console.log(Array.of(number1,number2,number3));
+
+
+// const finalNumber = [...number1,...number2,...number3];
+// console.log(finalNumber)
+
+
+// const finalNumber = number1.concat(number2);
+// console.log(finalNumber)
+
+// const finalNumber = number3.push(number1);
+// console.log(finalNumber)
+
+
+// console.log(Array.of(myArray,myHeros));
+
+// const  anotherArray = [0,1,2,3,4,[4,5,6],[5,2,4,6,[2,4,2,5]]];
+
+// const multiArray = anotherArray.flat(Infinity);
+
+// console.log(multiArray)
+
+console.log(Array.isArray(["Saurabh"]))
+console.log(Array.from("Saurabh"))
