75f27fdbcb8a4c4be23dc0e0597e6ed24a2e6689
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-07-31 20:35:01 +0530

    add some practice file here

--- class_8/dateandtime.js
+++ class_8/dateandtime.js
@@ -8,7 +8,16 @@
 // console.log(myCreatedDate.toString());
 // console.log(myCreatedDate.toLocaleDateString());
 // console.log(myCreatedDate.toLocaleString());
-let myTimeStamp = Date.now();
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
-console.log(myTimeStamp.floor(myTimeStamp / 1000));
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(myTimeStamp.floor(myTimeStamp / 1000));
+
+let newDate = new Date();
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getHours());
+console.log(newDate.getMinutes());
+console.log(newDate.getSeconds());
---
+++ class_9/array.js
@@ -0,0 +1,31 @@
+//Array 
+
+const myArray = [0,1,2,3,4,5];
+
+const myHeros = ["Ranbir Kapoor", "Iron Man", "Captain America", "Shaktiman"];
+
+// console.log(myHeros[2])
+
+// myHeros.push("Saurabh Yadav")
+
+// myHeros.pop()  i have See in this array have delete each name 
+// myHeros.pop()
+
+// console.log(myHeros)
+
+// myArray.unshift(7);
+
+// myArray.shift();
+
+// myArray.unshift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(4))
+
+// const sliceArray = myArray.slice(1,5);
+// console.log(sliceArray);
+// console.log(myArray);
+
+const newArray = myArray.splice(1 , 4);
+console.log(myArray);
+// console.log(newArray)
