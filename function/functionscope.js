7af6a9460ea8e40f67440fefb78025d3913223b2
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-08-08 21:03:57 +0530

    Practice functons scope and global and local varible

---
+++ function/functionscope.js
@@ -0,0 +1,16 @@
+// if (true) {
+    // const  user =  "saurabh";
+    // if (user === "saurabh") {
+        // const lastname = "yaduvanshi";    // The code snippet is commented out, but if it were to be executed, it would define a constant variable `user` with the value "saurabh" and then check if `user` is equal to "saurabh". If the condition is true, it would define another constant variable `lastname` with the value "yaduvanshi". Finally, it would attempt to log the concatenation of `lastname` and `user`, which would result in "yaduvanshisaurabh". However, since the code is commented out, nothing will be executed or logged.
+         // console.log(lastname + user);
+    // }
+// }
+
+
+const newValue = function (value){
+    return value + 10; 
+                       // The function `newValue` takes a parameter `value` and returns the result of adding `10` to it. In this case, if you call `newValue(5)`, it will return `15`.   
+}
+console.log(newValue(5));
+
+
