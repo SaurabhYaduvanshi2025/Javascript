c06d5eff9725ba07d9c9a6870d478e5063a1c9d6
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-08-25 09:51:04 +0530

    Practice of for each loop and map

---
+++ loop/loop.js
@@ -0,0 +1,56 @@
+// const array = ["Hero","bajaj","Royal Enfield", "TVS"];
+
+
+// for(const arrays of array ){
+
+    
+//     console.log(arrays);
+// }
+
+
+
+// const number = [1,2,3,4,5,6,7,8,9]
+
+
+
+// for(num of number){
+//     console.log(num)
+// }
+
+
+
+// Maps practice Sessions start from here
+
+
+// const map = new Map()
+
+// map.set('IN',"India")
+// map.set('USA',"United State of America")
+// map.set('fr',"France")
+
+
+// console.log(map)
+
+
+
+
+const map = new Map()
+
+map.set('UK' ,"Uttara Khand")
+map.set('UP', "Uttar Pradesh")
+map.set('DEL', "Delhi")
+
+
+
+// console.log (map)
+
+
+
+for(const [key,value] of map){
+
+    console.log(key, "=", value)
+
+}
+
+
+
