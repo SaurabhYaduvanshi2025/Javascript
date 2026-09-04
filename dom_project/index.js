a221eb7557746b0b6160805ce869af18350de329
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-04 17:55:13 +0530

    changes in Some File

--- dom_project/index.js
+++ dom_project/index.js
@@ -4,4 +4,7 @@ const body = document.querySelector('body');
 
 button.forEach(function(button){
     console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+    });
 });
