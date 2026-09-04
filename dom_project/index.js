e65f8d3a1735a7a2a8d95f41659e45c8b7ce04c0
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-04 22:33:08 +0530

    Practice of DOM with event handler

--- dom_project/index.js
+++ dom_project/index.js
@@ -1,10 +1,24 @@
-const button = document.querySelectorAll('.button');
+
+const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
-
-button.forEach(function(button){
-    console.log(button);
-    button.addEventListener('click', function(e){
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
         console.log(e);
+        console.log(e.target);
+
+
+        if(e.target.id === 'Yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'pink') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
     });
 });
