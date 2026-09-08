05393eb3840d645297563ac907d83ae551c6fb55
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-08 14:47:41 +0530

    practice of event

--- event/event.js
+++ event/event.js
@@ -1,26 +1,61 @@
+// const userForm = document.getElementById("userForm");
+
+// const username = document.getElementById("username");
+
+// const submit = document.getElementById("addUser");
+
+
+
+
+// username.addEventListener("input", (e)=>{
+//     console.log(e.target.value);
+
+// })
+
+
+// username.addEventListener("focus", (e)=>{
+
+//        console.log("Input field is focused")
+
+// })
+
+
+// userForm.addEventListener("submit", (e)=>{
+//     e.preventDefault();
+//     console.log("Form submitted");
+// })
+
+// eevent bubbling and capturing
+
 const userForm = document.getElementById("userForm");
-
 const username = document.getElementById("username");
+const userList = document.getElementById("userList");
 
-const submit = document.getElementById("addUser");
-
-
-
-
-username.addEventListener("input", (e)=>{
+username.addEventListener("input", (e) => {
     console.log(e.target.value);
+});
 
-})
+username.addEventListener("focus", () => {
+    console.log("Input field is focused");
+});
 
+userForm.addEventListener("submit", (e) => {
 
-username.addEventListener("focus", (e)=>{
-
-       console.log("Input field is focused")
-
-})
-
-
-userForm.addEventListener("submit", (e)=>{
     e.preventDefault();
-    console.log("Form submitted");
-})
+
+    const name = username.value;
+
+    const li = document.createElement("li");
+
+    li.textContent = name;
+
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "Delete";
+
+    li.append(deleteBtn);
+
+    userList.append(li);
+
+    username.value = "";
+});
