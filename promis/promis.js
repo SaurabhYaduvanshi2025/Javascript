87da7b2345196b6fb2a59b046808be54f3cfedcf
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-13 18:11:09 +0530

    practice of promis.js

--- promis/promis.js
+++ promis/promis.js
@@ -64,30 +64,55 @@ new Promise(function(resolve, reject){
 
 
 
-const promisFive = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if(!error){
-           resolve({username: "Saurabh Yadav", password: "Sau@1234"})
-        }else{
-            reject('Error: Js Went wrong')
-        }
-    },2000)
-}); 
+// const promisFive = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if(!error){
+//            resolve({username: "Saurabh Yadav", password: "Sau@1234"})
+//         }else{
+//             reject('Error: Js Went wrong')
+//         }
+//     },2000)
+// }); 
+//     // n
+//  async function consumePromiseFive(){
+//     try{    
+//             const response = await promisFive
+//     console.log(response)
 
- async function consumePromiseFive(){
-    try{
-            const response = await promisFive
-    console.log(response)
+//     } catch(error){
+//         console.log(error)
+//     }
+//  }
 
-    } catch(error){
-        console.log(error)
-    }
- }
+//  consumePromiseFive()
 
- consumePromiseFive()
+// how is working async try fetch a data Through API 
+// async function getallUsers() {
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data)
+//     }catch{
+//         console.log("E:" ,error);
+//     }
+// }
+
+// getallUsers()
 
 
 
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+
+.then((data)=>{
+      console.log(data);
+
+})
+.catch((error)=>{
+     console.log(error)
+})
 
 
