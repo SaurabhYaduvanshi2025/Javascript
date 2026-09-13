0c23c1135e70648a9583bd845a9ddb7acfe89377
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-13 15:38:35 +0530

    practice of Promise

--- promis/promis.js
+++ promis/promis.js
@@ -22,3 +22,72 @@ new Promise(function(resolve, reject){
 }).then(function(){
     console.log('promise consumed'); // this will run after the promise is resolved
     })
+
+
+ const promisThree = new Promise(function(resolve,reject){          // in  this line i build promisthree for print user and  email id ,
+
+     setTimeout(function(){
+     
+        resolve({username: "chai", email:"chaipilo@gmail.com"});
+
+     },1000)
+
+
+ });
+
+ promisThree.then(function(user){
+    console.log(user)
+ })
+
+
+
+//  const promisFour = new Promise(function(resolve,reject){ 
+//     setTimeout(function(){
+//         let error = false    
+//         if (!error){
+//             resolve({username: "Saurabh Yadav", password: "kafir@9899"})
+//         }else{
+//             reject('Error Something went wrong')
+//         }
+//     },1000)
+//  })
+
+
+//  promisFour.then((user)=>{
+//     return user.username
+//  }).then((username)=>{
+//     console.log(username)
+//  }).catch(function(error){
+//       console.log(error)
+//  }).finally(()=>console.log("finall")) 
+
+
+
+
+const promisFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+           resolve({username: "Saurabh Yadav", password: "Sau@1234"})
+        }else{
+            reject('Error: Js Went wrong')
+        }
+    },2000)
+}); 
+
+ async function consumePromiseFive(){
+    try{
+            const response = await promisFive
+    console.log(response)
+
+    } catch(error){
+        console.log(error)
+    }
+ }
+
+ consumePromiseFive()
+
+
+
+
+
