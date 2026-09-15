184ee6cfc9739f1e97df3606718fbec0bee85ed5
saurabh yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-09-15 06:52:21 +0530

    Practice of construct

---
+++ constructor/constru.js
@@ -0,0 +1,51 @@
+// class User {
+//     constructor(username,email,password){   //
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+          
+// encryptPassword(){
+//     return `${this.password}abc`
+// }
+
+// ChangeUsername(){ 
+//     return `${this.username.toUpperCase()}`
+// }
+
+// ChangeEmail(){
+//     return `${this.email}`  
+// }
+
+// }
+
+
+// const chai =  new User("saurabh", "saurabh@gmail.com", "12345");
+// console.log(chai.encryptPassword())
+// console.log(chai.ChangeUsername())
+// console.log(chai.ChangeEmail())
+
+
+
+function User ( username,email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+
+}
+
+User.prototype.email = function(){
+    return `${this.email}`
+}
+
+
+const tea = new User("Tea", "test@gmail.com","123456")
+
+console.log(tea.encryptPassword());
+console.log(tea.username)
